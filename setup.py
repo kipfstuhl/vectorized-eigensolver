66646695d27e65db0e02336c64c9ae379ae731c0
Jonas Kipfstuhl <jonas.kipfstuhl@t-online.de>
2020-07-14 11:45:28 +0200

    fix numpy includes

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy
 
 setup(
     name = "python_interface",
@@ -9,6 +10,7 @@ setup(
     ext_modules = [
     Extension(name="python_interface", sources=["python_interface.pyx"],
               libraries = ["m"], language="c++",
+              include_dirs=[numpy.get_include()],
               extra_compile_args=['-fopenmp', '-O3'],
               extra_link_args=['-fopenmp'],
               define_macros=[('OPENMP', None)])
